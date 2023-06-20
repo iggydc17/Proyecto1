e34239ba4297bbd2d346bf429cbc1d5048909027
Ignacio A. Gonzalez <iggy.dc17@gmail.com>
2023-06-20 16:51:48 -0600

    Se agregaron los datos del admin

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -4,8 +4,8 @@ from .views import inicio, cursos, entregables, estudiantes, profesores
 
 urlpatterns = [
 
-    path('inicio/', inicio),
-    path('cursos/', cursos),
+    path('inicio/', inicio, name="Inicio"),
+    path('cursos/', cursos, name="Cursos"),
     path('entregables/', entregables, name="Entregables"),
     path('estudiantes/', estudiantes, name="Estudiantes"),
     path('profesores/', profesores, name="Profesores"),
